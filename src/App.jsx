0d952fd00ffd68b822f4be577d4b4700c172229e
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2023-05-27 17:31:25 +0530

    fetch posts by id and polling interval

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
 //import "./App.css";
-import { useGetAllPostQuery } from "./services/posts";
+import {
+  useGetAllPostQuery,
+  useGetPostByIdQuery,
+  useGetPostByLimitQuery,
+} from "./services/posts";
 
 function App() {
   // Get all data
   //const responseInfo = useGetAllPostQuery();
   //console.log("data", responseInfo);
-  const { data, isError, isLoading, isFetching, isSuccess } =
-    useGetAllPostQuery();
+  // const { data, isError, isFetching, isLoading, isSuccess } =
+  //   useGetPostByIdQuery(7);
+  const { data, isError, isFetching, isLoading, isSuccess } =
+    useGetPostByLimitQuery(5, {
+      pollingInterval: 3000,
+    });
   return (
     <>
       <div>
-        <h2>RTK Query (Get all data)</h2>
+        <h2>RTK Query Get single data by Id</h2>
         {isLoading && <h2>...Loading</h2>}
         {isFetching && <h2>...Fetching</h2>}
         {isError && <h2>something went wrong</h2>}
-        {isSuccess &&
+        {isSuccess && (
           // get all data
-          data.map((post) => (
-            <div key={post.id}>
-              <h2>
-                {post.id} {post.title}
-              </h2>
-              <p>{post.body}</p>
-              <hr />
-            </div>
-          ))}
+          // <div>
+          //   <div>
+          //     <h2>
+          //       {data.id} {data.title}
+          //     </h2>
+          //     <p>{data.body}</p>
+          //   </div>
+          // </div>
+          <div>
+            {
+              // get limited data
+              data.map((post, i) => (
+                <div key={i}>
+                  <h2>
+                    {post.id} {post.title}
+                  </h2>
+                  <p>{post.body}</p>
+                  <hr />
+                </div>
+              ))
+            }
+          </div>
+        )}
       </div>
     </>
   );
