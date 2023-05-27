c4ce0402b0d87ccd54eb1cecae34eb2cf0e7ab0b
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2023-05-27 17:15:11 +0530

    fetch all posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import "./App.css";
+//import "./App.css";
+import { useGetAllPostQuery } from "./services/posts";
 
 function App() {
+  // Get all data
+  //const responseInfo = useGetAllPostQuery();
+  //console.log("data", responseInfo);
+  const { data, isError, isLoading, isFetching, isSuccess } =
+    useGetAllPostQuery();
   return (
     <>
-      <h2>RTK Query</h2>
+      <div>
+        <h2>RTK Query (Get all data)</h2>
+        {isLoading && <h2>...Loading</h2>}
+        {isFetching && <h2>...Fetching</h2>}
+        {isError && <h2>something went wrong</h2>}
+        {isSuccess &&
+          // get all data
+          data.map((post) => (
+            <div key={post.id}>
+              <h2>
+                {post.id} {post.title}
+              </h2>
+              <p>{post.body}</p>
+              <hr />
+            </div>
+          ))}
+      </div>
     </>
   );
 }
